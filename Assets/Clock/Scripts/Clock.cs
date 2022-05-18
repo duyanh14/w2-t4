5e9fc8d545dbcfd094fbbd16e4e9248c6a0690d7
duyanh14 <duyanhnguyen2001@gmail.com>
2022-05-19 00:04:34 +0700

    Remove conflict

--- Assets/Clock/Scripts/Clock.cs
+++ Assets/Clock/Scripts/Clock.cs
@@ -16,45 +16,19 @@ public class Clock : MonoBehaviour
 
     public bool Continuous;
 
-<<<<<<< HEAD
-    private void Awake()
-=======
     public int AddHour = 0;
 
     DateTime date;
 
     void Start()
->>>>>>> d122be9c7dd11235b4e21e63b29e2d6ed4e24cf9
     {
         HourHandTransform = transform.Find("HourHand");
         MinuteHandTransform = transform.Find("MinuteHand");
         SecondHandTransform = transform.Find("SecondHand");
-<<<<<<< HEAD
-
-        DateTime date = DateTime.Now;
-
-        HourHandTransform.eulerAngles = new Vector3(0, 0, date.Hour * DegreesPerHour);
-        MinuteHandTransform.eulerAngles = new Vector3(0, 0, date.Minute * DegreesPerMinute);
-        SecondHandTransform.eulerAngles = new Vector3(0, 0, date.Second * DegreesPerSecond);
-=======
->>>>>>> d122be9c7dd11235b4e21e63b29e2d6ed4e24cf9
     }
 
     void Update()
     {
-<<<<<<< HEAD
-        if (Continuous)
-        {
-            TimeSpan date = DateTime.Now.TimeOfDay;
-            HourHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TotalHours * DegreesPerHour);
-            MinuteHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TotalMinutes * DegreesPerMinute);
-            SecondHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TotalSeconds * DegreesPerSecond);
-        }
-        else
-        {
-            DateTime date = DateTime.Now;
-
-=======
         date = DateTime.Now;
         if (AddHour > 0)
         {
@@ -68,7 +42,6 @@ public class Clock : MonoBehaviour
         }
         else
         {
->>>>>>> d122be9c7dd11235b4e21e63b29e2d6ed4e24cf9
             HourHandTransform.eulerAngles = new Vector3(0, 0, date.Hour * DegreesPerHour);
             MinuteHandTransform.eulerAngles = new Vector3(0, 0, date.Minute * DegreesPerMinute);
             SecondHandTransform.eulerAngles = new Vector3(0, 0, date.Second * DegreesPerSecond);
