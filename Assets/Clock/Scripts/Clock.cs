b75faa68834b7dff7caec8a170b298ed1390d4bd
duyanh14 <duyanhnguyen2001@gmail.com>
2022-05-18 18:32:07 +0700

    add_more_clocks

---
+++ Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    const float DegreesPerHour = 30f;
+    const float DegreesPerMinute = 6f;
+    const float DegreesPerSecond = 6f;
+
+    private Transform HourHandTransform;
+    private Transform MinuteHandTransform;
+    private Transform SecondHandTransform;
+
+    public bool Continuous;
+
+    public int AddHour = 0;
+
+    DateTime date;
+
+    private void Awake()
+    {
+        HourHandTransform = transform.Find("HourHand");
+        MinuteHandTransform = transform.Find("MinuteHand");
+        SecondHandTransform = transform.Find("SecondHand");
+
+        date = DateTime.Now;
+
+        HourHandTransform.eulerAngles = new Vector3(0, 0, date.Hour * DegreesPerHour);
+        MinuteHandTransform.eulerAngles = new Vector3(0, 0, date.Minute * DegreesPerMinute);
+        SecondHandTransform.eulerAngles = new Vector3(0, 0, date.Second * DegreesPerSecond);
+    }
+
+    void Update()
+    {
+        date = DateTime.Now;
+        if (AddHour > 0)
+        {
+            date = date.AddHours(AddHour);
+        }
+        if (Continuous)
+        {
+            HourHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TimeOfDay.TotalHours * DegreesPerHour);
+            MinuteHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TimeOfDay.TotalMinutes * DegreesPerMinute);
+            SecondHandTransform.eulerAngles = new Vector3(0, 0, (float)date.TimeOfDay.TotalSeconds * DegreesPerSecond);
+        }
+        else
+        {
+            HourHandTransform.eulerAngles = new Vector3(0, 0, date.Hour * DegreesPerHour);
+            MinuteHandTransform.eulerAngles = new Vector3(0, 0, date.Minute * DegreesPerMinute);
+            SecondHandTransform.eulerAngles = new Vector3(0, 0, date.Second * DegreesPerSecond);
+        }
+    }
+
+}
